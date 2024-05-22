daf9428fe85703be5a94be461127bc2b2ff41018
San-tito <68120901+San-tito@users.noreply.github.com>
2024-05-22 02:03:16 +0200

    ➕ locura

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 15:02:14 by sguzman           #+#    #+#             */
-/*   Updated: 2024/05/22 01:29:56 by sguzman          ###   ########.fr       */
+/*   Updated: 2024/05/22 02:02:47 by sguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,27 @@ static int	ft_isdigit(int c)
 	return (c >= 0x30 && c <= 0x39);
 }
 
-static int	ft_isspace(int c)
+static void	whitespace(char **str)
 {
-	return (c == 0x20 || c == 0xD || c == 0xA || c == 0x9);
+	if (str == NULL || *str == NULL)
+		return ;
+	while (**str == 0x20 || **str == 0xD || **str == 0xA || **str == 0x9)
+		(*str)++;
 }
 
-ssize_t	ft_strtoi(const char *s, char **endptr)
+ssize_t	ft_strtoi(const char *nptr, char **endptr)
 {
-	ssize_t	acc;
-	int		neg;
+	ssize_t		acc;
+	int			neg;
+	const char	*s = nptr;
 
-	while (ft_isspace(*s))
-		s++;
-	neg = 0;
-	if (*s == '-')
-		neg = 1;
+	acc = 0;
+	whitespace((char **)&s);
+	neg = (*s == '-');
 	if (*s == '-' || *s == '+')
 		s++;
-	acc = 0;
+	if (ft_isdigit(*s) == 0 && endptr != 0)
+		return (*endptr = (char *)nptr, acc);
 	while (ft_isdigit(*s))
 	{
 		if (neg && (acc < (INT_MIN + (*s - '0')) / 10))
@@ -64,8 +67,7 @@ int	legal_number(const char *string, int *result)
 	value = ft_strtoi(string, &ep);
 	if (errno || ep == string)
 		return (0);
-	while (ft_isspace(*ep))
-		ep++;
+	whitespace(&ep);
 	if (*string && *ep == '\0')
 	{
 		if (result)
