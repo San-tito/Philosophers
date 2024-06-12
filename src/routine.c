e309ee9014e1cb8b5337128afa65ede29032d1d5
San-tito <68120901+San-tito@users.noreply.github.com>
2024-06-12 14:56:00 +0200

    🔥 increible

--- src/routine.c
+++ src/routine.c
@@ -6,17 +6,38 @@
 /*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 17:26:21 by sguzman           #+#    #+#             */
-/*   Updated: 2024/06/12 14:36:45 by sguzman          ###   ########.fr       */
+/*   Updated: 2024/06/12 14:55:01 by sguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	log_state(const char *action, t_philo philo, t_table table)
+void	log_state(const char *state, t_philo philo, t_table table)
 {
 	pthread_mutex_lock(&table.log_lock);
-	printf("%ld %d %s\n", current_time() - table.start_time, philo.id, action);
+	printf("%ld %d %s\n", current_time() - table.start_time, philo.id, state);
 	pthread_mutex_unlock(&table.log_lock);
+}
+
+void	dine(t_philo philo, t_table table)
+{
+	pthread_mutex_lock(philo.first_fork);
+	log_state("has taken a fork", philo, table);
+	pthread_mutex_lock(philo.second_fork);
+	log_state("has taken a fork", philo, table);
+	log_state("is eating", philo, table);
+	philo.last_meal = current_time();
+	sleep_for(table.time_eat);
+	philo.meal_count++;
+	pthread_mutex_unlock(philo.first_fork);
+	pthread_mutex_unlock(philo.second_fork);
+}
+
+void	rest(t_philo philo, t_table table)
+{
+	log_state("is sleeping", philo, table);
+	sleep_for(table.time_sleep);
+	log_state("is thinking", philo, table);
 }
 
 void	*philosopher(void *arg)
@@ -30,19 +51,8 @@ void	*philosopher(void *arg)
 		continue ;
 	while (42)
 	{
-		pthread_mutex_lock(philo.first_fork);
-		log_state("has taken a fork", philo, table);
-		pthread_mutex_lock(philo.second_fork);
-		log_state("has taken a fork", philo, table);
-		log_state("is eating", philo, table);
-		philo.last_meal = current_time();
-		sleep_for(table.time_eat);
-		philo.meal_count++;
-		pthread_mutex_unlock(philo.first_fork);
-		pthread_mutex_unlock(philo.second_fork);
-		log_state("is sleeping", philo, table);
-		sleep_for(table.time_sleep);
-		log_state("is thinking", philo, table);
+		dine(philo, table);
+		rest(philo, table);
 	}
 	return (0);
 }
